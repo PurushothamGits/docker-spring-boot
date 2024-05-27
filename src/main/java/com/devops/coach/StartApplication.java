7ebdb4dedd646a3edf433c2269e0004c8b250666
PurushothamGits <108726129+PurushothamGits@users.noreply.github.com>
2024-05-27 18:04:20 +0200

    my changes

--- src/main/java/com/devops/coach/StartApplication.java
+++ src/main/java/com/devops/coach/StartApplication.java
@@ -12,6 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
+        model.addAttribute("msg", "This is Purushotham Medasani");
         model.addAttribute("title", "Welcome folks..We are learning Kubernete Deployment using Helm, Jenkins Pipeline Today's date is Feb 18th..!!!! ");
         model.addAttribute("msg", "Hello All..We are deploying springboot application into EKS cluster using Helm + Jenkins Pipeline!!!!");
         return "index";
